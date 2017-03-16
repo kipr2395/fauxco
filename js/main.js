0b855a7b9507ef859a8a945eab2c9dcd26da7c05
Kimberly Preston <kimberlypreston@rgnt2-86-157-dhcp.int.colorado.edu>
2017-03-16 09:07:54 -0600

    final

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+
+
+	$(".na").click(function(){
+		$("*").animate({ scrollTop: 650 }, 'slow');
+		$("*").scrollTop(650);
+		
+	});
+
+	$(".ns").click(function(){
+		$("*").animate({ scrollTop: 1900 }, 'slow');
+		$("*").scrollTop(1900);
+		
+	});
+
+	$(".nf").click(function(){
+		$("*").animate({ scrollTop: 4090 }, 'slow');
+		$("*").scrollTop(4090);
+		
+	});
+
+	$(".nc").click(function(){
+		$("*").animate({ scrollTop: 5600 }, 'slow');
+		$("*").scrollTop(5600);
+		
+	});	
+
+
+
+});
+
